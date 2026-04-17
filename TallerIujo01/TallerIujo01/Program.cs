b934e48ed9bcb2e3da10193ca7078e46b723e5d2
joselynvleal-cmd <pablo36115@gmail.com>
2026-04-17 11:39:01 -0400

    Agregando manipulacion de texto

--- TallerIujo01/TallerIujo01/Program.cs
+++ TallerIujo01/TallerIujo01/Program.cs
@@ -14,9 +14,26 @@ namespace TallerIujo01
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("=== Taller 01 ===");
+			 
+			// 1. El dato del usuario
+			string registroUsuario = "	ID_77;	juanperez;	EVALUACION; 	95";
 			
-			// TODO: Implement Functionality Here
+			Console.WriteLine(registroUsuario);
+			string registroLimpio = registroUsuario.Trim();
+			Console.WriteLine(registroLimpio);
+			
+			string[] partes = registroLimpio.Split(';');
+			string id = partes[0].Trim();
+			string nombre = partes[1].Trim();
+			string descripcion = partes[2].Trim();
+			string nota = partes[3].Trim();
+			
+			Console.WriteLine(string.Format("El ID es: {0} del usuario {1} descripcion {2} con la nota {3}", id, nombre, descripcion, nota));
+			
+			// Flujo de Archivos
+			
+			//string rutaraiz = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DatosIUJO")
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
